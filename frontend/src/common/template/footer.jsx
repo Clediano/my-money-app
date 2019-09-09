98be444bbeb6a12c133ee6d764fc67d6ed5e324e
Clediano Estefenon <clediano@microsys.inf.br>
2019-09-09 10:55:47 -0300

    first commit

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'> 
+        <strong> 
+            Copyright &copy; 2017
+            <a href='http://microsys.inf.br' target='_blank'> Microsys</a>.
+        </strong>
+    </footer>
+)
